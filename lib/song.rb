a029affcfe236e64082f385751488bac00f4dbc9
Ben Hayes <b.t.hayes@comcast.net>
2021-01-03 20:37:16 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song 
+  
+  @@count = 0 
+  @@genres = []
+  
+  attr_accessor :name, :artist, :genre 
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@genres << genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres 
+    
+  
+end
